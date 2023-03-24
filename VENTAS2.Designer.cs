a97b229e9bbe7a4e52f6c6ac2d98a07eb4aeae45
Néstor Alberto Olivas Valdez <N3al2003@gmail.com>
2023-03-24 10:50:48 -0700

    PRUEBA

--- VENTAS2.Designer.cs
+++ VENTAS2.Designer.cs
@@ -54,10 +54,11 @@ namespace BASEDEDATOSPC2
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("AR JULIAN", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(330, 23);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(495, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 32);
+            this.label1.Size = new System.Drawing.Size(189, 47);
             this.label1.TabIndex = 51;
             this.label1.Text = "VENTAS";
             // 
@@ -66,143 +67,160 @@ namespace BASEDEDATOSPC2
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 58);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 89);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(782, 117);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1173, 180);
             this.dataGridView1.TabIndex = 50;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(674, 254);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1011, 391);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.Size = new System.Drawing.Size(144, 25);
             this.label3.TabIndex = 129;
             this.label3.Text = "ID ALMACEN";
             // 
             // TXTIDALMACEN
             // 
-            this.TXTIDALMACEN.Location = new System.Drawing.Point(677, 273);
+            this.TXTIDALMACEN.Location = new System.Drawing.Point(1016, 420);
+            this.TXTIDALMACEN.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TXTIDALMACEN.Name = "TXTIDALMACEN";
-            this.TXTIDALMACEN.Size = new System.Drawing.Size(121, 20);
+            this.TXTIDALMACEN.Size = new System.Drawing.Size(180, 26);
             this.TXTIDALMACEN.TabIndex = 128;
             this.TXTIDALMACEN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXTIDALMACEN_KeyPress);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(543, 254);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(814, 391);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.Size = new System.Drawing.Size(130, 25);
             this.label2.TabIndex = 127;
             this.label2.Text = "ID CLIENTE";
             // 
             // TXTIDCLIENTE
             // 
-            this.TXTIDCLIENTE.Location = new System.Drawing.Point(546, 273);
+            this.TXTIDCLIENTE.Location = new System.Drawing.Point(819, 420);
+            this.TXTIDCLIENTE.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TXTIDCLIENTE.Name = "TXTIDCLIENTE";
-            this.TXTIDCLIENTE.Size = new System.Drawing.Size(121, 20);
+            this.TXTIDCLIENTE.Size = new System.Drawing.Size(180, 26);
             this.TXTIDCLIENTE.TabIndex = 126;
             this.TXTIDCLIENTE.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXTIDCLIENTE_KeyPress);
             // 
             // IDPRO
             // 
             this.IDPRO.AutoSize = true;
-            this.IDPRO.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IDPRO.Location = new System.Drawing.Point(412, 254);
+            this.IDPRO.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDPRO.Location = new System.Drawing.Point(618, 391);
+            this.IDPRO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.IDPRO.Name = "IDPRO";
-            this.IDPRO.Size = new System.Drawing.Size(58, 16);
+            this.IDPRO.Size = new System.Drawing.Size(93, 25);
             this.IDPRO.TabIndex = 125;
             this.IDPRO.Text = "MONTO";
             // 
             // TXTMONTO
             // 
-            this.TXTMONTO.Location = new System.Drawing.Point(415, 273);
+            this.TXTMONTO.Location = new System.Drawing.Point(622, 420);
+            this.TXTMONTO.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TXTMONTO.Name = "TXTMONTO";
-            this.TXTMONTO.Size = new System.Drawing.Size(121, 20);
+            this.TXTMONTO.Size = new System.Drawing.Size(180, 26);
             this.TXTMONTO.TabIndex = 124;
             this.TXTMONTO.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXTMONTO_KeyPress);
             // 
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label15.Location = new System.Drawing.Point(107, 254);
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(160, 391);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(47, 16);
+            this.label15.Size = new System.Drawing.Size(75, 25);
             this.label15.TabIndex = 123;
             this.label15.Text = "SERIE";
             // 
             // TXTSERIE
             // 
-            this.TXTSERIE.Location = new System.Drawing.Point(110, 273);
+            this.TXTSERIE.Location = new System.Drawing.Point(165, 420);
+            this.TXTSERIE.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TXTSERIE.Name = "TXTSERIE";
-            this.TXTSERIE.Size = new System.Drawing.Size(121, 20);
+            this.TXTSERIE.Size = new System.Drawing.Size(180, 26);
             this.TXTSERIE.TabIndex = 122;
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Location = new System.Drawing.Point(10, 254);
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(15, 391);
+            this.label16.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(48, 16);
+            this.label16.Size = new System.Drawing.Size(77, 25);
             this.label16.TabIndex = 121;
             this.label16.Text = "FOLIO";
             // 
             // TXTFOLIO
             // 
-            this.TXTFOLIO.Location = new System.Drawing.Point(13, 273);
+            this.TXTFOLIO.Location = new System.Drawing.Point(20, 420);
+            this.TXTFOLIO.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TXTFOLIO.Name = "TXTFOLIO";
-            this.TXTFOLIO.Size = new System.Drawing.Size(57, 20);
+            this.TXTFOLIO.Size = new System.Drawing.Size(84, 26);
             this.TXTFOLIO.TabIndex = 120;
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(252, 254);
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(378, 391);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(156, 16);
+            this.label13.Size = new System.Drawing.Size(237, 25);
             this.label13.TabIndex = 119;
             this.label13.Text = "ID TIPO DOCUMENTO";
             // 
             // TXTISTIPODOCUMENTO
             // 
-            this.TXTISTIPODOCUMENTO.Location = new System.Drawing.Point(255, 273);
+            this.TXTISTIPODOCUMENTO.Location = new System.Drawing.Point(382, 420);
+            this.TXTISTIPODOCUMENTO.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TXTISTIPODOCUMENTO.Name = "TXTISTIPODOCUMENTO";
-            this.TXTISTIPODOCUMENTO.Size = new System.Drawing.Size(121, 20);
+            this.TXTISTIPODOCUMENTO.Size = new System.Drawing.Size(180, 26);
             this.TXTISTIPODOCUMENTO.TabIndex = 118;
             this.TXTISTIPODOCUMENTO.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXTISTIPODOCUMENTO_KeyPress);
             // 
             // DATETIMEFECHA
             // 
-            this.DATETIMEFECHA.Location = new System.Drawing.Point(13, 331);
+            this.DATETIMEFECHA.Location = new System.Drawing.Point(20, 509);
+            this.DATETIMEFECHA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DATETIMEFECHA.Name = "DATETIMEFECHA";
-            this.DATETIMEFECHA.Size = new System.Drawing.Size(200, 20);
+            this.DATETIMEFECHA.Size = new System.Drawing.Size(298, 26);
             this.DATETIMEFECHA.TabIndex = 135;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(10, 312);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 480);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.Size = new System.Drawing.Size(85, 25);
             this.label6.TabIndex = 134;
             this.label6.Text = "FECHA";
             // 
             // BTNELIMINAR
             // 
             this.BTNELIMINAR.BackColor = System.Drawing.Color.Red;
-            this.BTNELIMINAR.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNELIMINAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTNELIMINAR.ForeColor = System.Drawing.Color.Black;
-            this.BTNELIMINAR.Location = new System.Drawing.Point(443, 181);
+            this.BTNELIMINAR.Location = new System.Drawing.Point(664, 278);
+            this.BTNELIMINAR.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BTNELIMINAR.Name = "BTNELIMINAR";
-            this.BTNELIMINAR.Size = new System.Drawing.Size(88, 49);
+            this.BTNELIMINAR.Size = new System.Drawing.Size(132, 75);
             this.BTNELIMINAR.TabIndex = 138;
             this.BTNELIMINAR.Text = "Eliminar";
             this.BTNELIMINAR.UseVisualStyleBackColor = false;
@@ -210,11 +228,12 @@ namespace BASEDEDATOSPC2
             // BTNGUARDAR
             // 
             this.BTNGUARDAR.BackColor = System.Drawing.Color.LightGreen;
-            this.BTNGUARDAR.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNGUARDAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTNGUARDAR.ForeColor = System.Drawing.Color.Black;
-            this.BTNGUARDAR.Location = new System.Drawing.Point(349, 181);
+            this.BTNGUARDAR.Location = new System.Drawing.Point(524, 278);
+            this.BTNGUARDAR.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BTNGUARDAR.Name = "BTNGUARDAR";
-            this.BTNGUARDAR.Size = new System.Drawing.Size(88, 49);
+            this.BTNGUARDAR.Size = new System.Drawing.Size(132, 75);
             this.BTNGUARDAR.TabIndex = 137;
             this.BTNGUARDAR.Text = "Guardar";
             this.BTNGUARDAR.UseVisualStyleBackColor = false;
@@ -222,21 +241,22 @@ namespace BASEDEDATOSPC2
             // BTNBUSCAR
             // 
             this.BTNBUSCAR.BackColor = System.Drawing.Color.Gold;
-            this.BTNBUSCAR.Font = new System.Drawing.Font("AR JULIAN", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNBUSCAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTNBUSCAR.ForeColor = System.Drawing.Color.Black;
-            this.BTNBUSCAR.Location = new System.Drawing.Point(255, 181);
+            this.BTNBUSCAR.Location = new System.Drawing.Point(344, 485);
+            this.BTNBUSCAR.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BTNBUSCAR.Name = "BTNBUSCAR";
-            this.BTNBUSCAR.Size = new System.Drawing.Size(88, 49);
+            this.BTNBUSCAR.Size = new System.Drawing.Size(132, 75);
             this.BTNBUSCAR.TabIndex = 136;
             this.BTNBUSCAR.Text = "Buscar";
             this.BTNBUSCAR.UseVisualStyleBackColor = false;
             // 
             // VENTAS2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.BTNELIMINAR);
             this.Controls.Add(this.BTNGUARDAR);
             this.Controls.Add(this.BTNBUSCAR);
@@ -256,6 +276,7 @@ namespace BASEDEDATOSPC2
             this.Controls.Add(this.TXTISTIPODOCUMENTO);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "VENTAS2";
             this.Text = "VENTAS2";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
