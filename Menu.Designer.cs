6164c82912e0f5765e185754cb85e669f177328c
Néstor Alberto Olivas Valdez <N3al2003@gmail.com>
2023-03-27 07:33:47 -0700

    Menu terminado

---
+++ Menu.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace BASEDEDATOSPC2
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAlmacenes = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnUnidad = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnProveedores = new System.Windows.Forms.Button();
+            this.btnCompras = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menu";
+            // 
+            // btnAlmacenes
+            // 
+            this.btnAlmacenes.Location = new System.Drawing.Point(93, 121);
+            this.btnAlmacenes.Name = "btnAlmacenes";
+            this.btnAlmacenes.Size = new System.Drawing.Size(118, 49);
+            this.btnAlmacenes.TabIndex = 1;
+            this.btnAlmacenes.Text = "Almacenes";
+            this.btnAlmacenes.UseVisualStyleBackColor = true;
+            this.btnAlmacenes.Click += new System.EventHandler(this.btnAlmacenes_Click);
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.Location = new System.Drawing.Point(217, 121);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(118, 49);
+            this.btnProductos.TabIndex = 2;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = true;
+            this.btnProductos.Click += new System.EventHandler(this.btnProductos_Click);
+            // 
+            // btnUnidad
+            // 
+            this.btnUnidad.Location = new System.Drawing.Point(341, 121);
+            this.btnUnidad.Name = "btnUnidad";
+            this.btnUnidad.Size = new System.Drawing.Size(118, 49);
+            this.btnUnidad.TabIndex = 3;
+            this.btnUnidad.Text = "Unidad";
+            this.btnUnidad.UseVisualStyleBackColor = true;
+            this.btnUnidad.Click += new System.EventHandler(this.btnUnidad_Click);
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Location = new System.Drawing.Point(149, 176);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(118, 49);
+            this.btnClientes.TabIndex = 4;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            this.btnClientes.Click += new System.EventHandler(this.btnClientes_Click);
+            // 
+            // btnProveedores
+            // 
+            this.btnProveedores.Location = new System.Drawing.Point(283, 176);
+            this.btnProveedores.Name = "btnProveedores";
+            this.btnProveedores.Size = new System.Drawing.Size(118, 49);
+            this.btnProveedores.TabIndex = 5;
+            this.btnProveedores.Text = "Proveedores";
+            this.btnProveedores.UseVisualStyleBackColor = true;
+            this.btnProveedores.Click += new System.EventHandler(this.btnProveedores_Click);
+            // 
+            // btnCompras
+            // 
+            this.btnCompras.Location = new System.Drawing.Point(149, 231);
+            this.btnCompras.Name = "btnCompras";
+            this.btnCompras.Size = new System.Drawing.Size(118, 49);
+            this.btnCompras.TabIndex = 6;
+            this.btnCompras.Text = "Compras";
+            this.btnCompras.UseVisualStyleBackColor = true;
+            this.btnCompras.Click += new System.EventHandler(this.btnCompras_Click);
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.Location = new System.Drawing.Point(283, 231);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(118, 49);
+            this.btnVentas.TabIndex = 7;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            this.btnVentas.Click += new System.EventHandler(this.btnVentas_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(548, 390);
+            this.Controls.Add(this.btnVentas);
+            this.Controls.Add(this.btnCompras);
+            this.Controls.Add(this.btnProveedores);
+            this.Controls.Add(this.btnClientes);
+            this.Controls.Add(this.btnUnidad);
+            this.Controls.Add(this.btnProductos);
+            this.Controls.Add(this.btnAlmacenes);
+            this.Controls.Add(this.label1);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAlmacenes;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnUnidad;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnProveedores;
+        private System.Windows.Forms.Button btnCompras;
+        private System.Windows.Forms.Button btnVentas;
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BASEDEDATOSPC2
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAlmacenes_Click(object sender, EventArgs e)
+        {
+            ALMACENES form = new ALMACENES();
+            form.ShowDialog();
+        }
+
+        private void btnProductos_Click(object sender, EventArgs e)
+        {
+            PRODUCTOS form = new PRODUCTOS();
+            form.ShowDialog();
+        }
+
+        private void btnUnidad_Click(object sender, EventArgs e)
+        {
+            UNIDAD form = new UNIDAD();
+            form.ShowDialog();
+        }
+
+        private void btnClientes_Click(object sender, EventArgs e)
+        {
+            CLIENTES form = new CLIENTES();
+            form.ShowDialog();
+        }
+
+        private void btnProveedores_Click(object sender, EventArgs e)
+        {
+            PROVEEDORES form = new PROVEEDORES();
+            form.ShowDialog();
+        }
+
+        private void btnCompras_Click(object sender, EventArgs e)
+        {
+            COMPRAS form = new COMPRAS();
+            form.ShowDialog();
+        }
+
+        private void btnVentas_Click(object sender, EventArgs e)
+        {
+            VENTAS2 form = new VENTAS2();
+            form.ShowDialog();
+        }
+    }
+}
